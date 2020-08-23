06d6a7acb99ea3a39a18b276503eb72e05cbe984
Dima Shymkiv <dima.shymkiv@gmail.com>
2020-08-23 11:39:50 +0300

    Client/Server from Lab3

---
+++ Client/Client/Client.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Client
+    {
+        static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static List<string> text = new List<string>();
+        static void Main(string[] args)
+        {
+            StreamWriter stream = new StreamWriter("Log.txt");
+            string[] command = { "Correction", "Who","exit"};
+            
+            socket.Connect("127.0.0.1", 1039);
+            string message = "";
+            while (message != "exit")
+            {
+                message = Console.ReadLine();
+                if (command.Contains(message))
+                {
+                    if (message != "Correction")
+                    {
+                        SendMessage(message);
+                        Console.WriteLine(ReceiveAnswer());
+                    }
+                    else
+                        FileCorrection();
+                    Log(stream, message);
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect Command");
+                    Log(stream, "Incorrect Command");
+                }
+                
+            }
+            stream.Flush();
+            stream.Close();
+            Console.ReadLine();
+        }
+        static void SendMessage(string message)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(message);
+            socket.Send(buffer,message.Length,SocketFlags.None);
+        }
+        static string ReceiveAnswer()
+        {
+            byte[] buffer = new byte[40];
+            int size = socket.Receive(buffer);
+            string s = Encoding.ASCII.GetString(buffer,0,size);
+            return s;
+
+        } 
+        static void ReadText()
+        {
+            text.Clear();
+            StreamReader stream = new StreamReader("file.txt");
+            for (int i = 0; !stream.EndOfStream; i++)
+                text.Add(stream.ReadLine());
+            stream.Close();
+        }
+        static void FileCorrection()
+        {
+            ReadText();
+            bool changhed = false;
+            StreamWriter stream = new StreamWriter("file.txt");
+            for (int i = 0; i < text.Count; ++i)
+            {
+                SendMessage(i.ToString());
+                string str = ReceiveAnswer();
+                if (str.Contains("No changes"))
+                    stream.WriteLine(text[i]);
+                else
+                {
+                    stream.WriteLine(str.Substring(2));
+                    changhed = true;
+                }
+                    
+
+            }
+            stream.Flush();
+            stream.Close();
+            SendMessage("Correction");
+            if (changhed)
+                Console.WriteLine("File Changhed");
+            else
+                Console.WriteLine("No Changes");
+
+        }
+        static void Log(StreamWriter stream,string command)
+        {
+            DateTime localTime = DateTime.Now;
+            stream.WriteLine(localTime.ToString() + " Command : " + command);
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static List<string> text = new List<string>();
+        static void Main(string[] args)
+        {
+            StreamWriter stream = new StreamWriter("Log.txt");
+            socket.Bind(new IPEndPoint(IPAddress.Any, 1039));
+            socket.Listen(1);
+
+            ReadText();
+            ChangeLines();
+          
+            string message;
+            while (true)
+            {
+                Socket clientSocket = socket.Accept();
+                Console.WriteLine("Device Connected");
+
+                do
+                {
+                    try
+                    {
+                        ReceiveMessage(out message, clientSocket);
+                    }
+                    catch (Exception)
+                    {
+                        break;
+                    }
+                    Log(stream, message);
+                    if (message.Contains("exit"))
+                        break;
+                    DoCommand(message, clientSocket);
+                } while (true);
+                
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+                stream.Flush();
+                stream.Close();
+            }
+            
+        }
+        static void ReceiveMessage(out string message,Socket client)
+        {
+            byte[] buffer = new byte[10];
+            int size = client.Receive(buffer);
+            message = Encoding.ASCII.GetString(buffer,0,size);
+        }
+        static void SendAnswer(string answer,Socket client)
+        {
+            byte[] buffer;
+            buffer = Encoding.ASCII.GetBytes(answer);
+            client.Send(buffer,answer.Length,SocketFlags.None);
+        }
+
+        static string WhoInfo()
+        {
+            string answer = "Shymkiv Dmytro K-26\nFile Correction";
+            return answer;
+        }
+        static void DoCommand(string command, Socket client)
+        {
+            int lineIndex;
+            if (command == "Who")
+                SendAnswer(WhoInfo(), client);
+            else if (command == "Correction")
+                ReadText();  
+            else if (int.TryParse(command,out lineIndex))
+                SendNewLine(client, lineIndex);
+        }
+        static void ReadText()
+        {
+            StreamReader streamRead = new StreamReader("file.txt");
+            text.Clear();
+            for (int i = 0; !streamRead.EndOfStream; i++)
+                text.Add(streamRead.ReadLine());
+
+            streamRead.Close();
+        }
+        static void ChangeLines()
+        {
+            Console.WriteLine("Text changes : ");
+            
+            Random random = new Random();
+            string[] newLines = new string[] {"New Server text","Random Server text","Server answer text","Server string","Server line"};
+
+            StreamWriter streamWrite = new StreamWriter("file.txt");
+            
+            for (int i = 0; i < text.Count; i++)
+            {
+                if (random.Next(2) == 1)
+                {
+                    string line = newLines[random.Next(newLines.Length)];
+                    streamWrite.WriteLine(line);
+                    Console.WriteLine(i.ToString() + " " + line);
+                }
+                else
+                    streamWrite.WriteLine(text[i]);
+                
+            }
+            streamWrite.Flush();
+            streamWrite.Close();
+
+        }
+        static void SendNewLine(Socket client,int i)
+        {
+            StreamReader streamRead = new StreamReader("file.txt");
+            string s = streamRead.ReadLine();
+            for (int j = 0; j < i; j++)
+                s = streamRead.ReadLine();
+            if (i < text.Count)
+            {
+                if (!(text[i] == s))
+                    SendAnswer(i.ToString() + " " + s, client);
+                else
+                    SendAnswer("No changes", client);
+            }
+
+            streamRead.Close();
+        }
+        static void Log(StreamWriter stream, string command)
+        {
+            DateTime localTime = DateTime.Now;
+            stream.WriteLine(localTime.ToString() + " Command : " + command);
+        }
+    }
+}
